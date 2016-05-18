d12ec90f5d8cf3df10ba411a57327643ccd2452f
Darius Kramer <dev@dkramer.fastmail.com>
2016-05-18 19:16:38 +0200

    fix forgotten header

--- includes/lib42.h
+++ includes/lib42.h
@@ -6,7 +6,7 @@
 /*   By: djean <djean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/18 17:27:01 by djean             #+#    #+#             */
-/*   Updated: 2016/05/18 19:07:12 by djean            ###   ########.fr       */
+/*   Updated: 2016/05/18 19:16:26 by djean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@
 # include "memory_42.h"
 # include "string_42.h"
 # include "vector_42.h"
+# include "buffer_42.h"
 
 #endif
