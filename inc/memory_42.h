b8a31cbac6a97898e41f3250cad3fc3520b5efbb
Alexandre Dubois <alexandre.dubois@goxheaven.ch>
2016-05-25 14:42:13 +0200

    ft_memcpy uses now her domain header, who includes the system header

--- inc/memory_42.h
+++ inc/memory_42.h
@@ -6,7 +6,7 @@
 /*   By: djean <djean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/18 16:45:36 by djean             #+#    #+#             */
-/*   Updated: 2016/05/24 16:33:01 by adubois          ###   ########.fr       */
+/*   Updated: 2016/05/25 14:41:29 by adubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define MEMORY_42_H
 
 # include <stdlib.h>
+# include <string.h>
 
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
