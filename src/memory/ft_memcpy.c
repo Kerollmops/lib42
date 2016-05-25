80cc8e370764cbcd9f81f1310e45b220b5c30fec
Alexandre Dubois <alexandre.dubois@goxheaven.ch>
2016-05-25 12:21:21 +0200

    Changed the header to be more specific

--- src/memory/ft_memcpy.c
+++ src/memory/ft_memcpy.c
@@ -6,11 +6,11 @@
 /*   By: adubois <adubois@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/12 17:49:56 by adubois           #+#    #+#             */
-/*   Updated: 2016/05/25 10:59:32 by adubois          ###   ########.fr       */
+/*   Updated: 2016/05/25 12:20:54 by adubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "lib42.h"
+#include <string.h>
 
 static void	ft_memcpy_align(void **dest, const void **src, size_t *n)
 {
