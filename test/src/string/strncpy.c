f5b4f176766140c186c2d5e2c9307d36b3c82fa5
Alexandre Dubois <alexandre.dubois@goxheaven.ch>
2016-05-26 14:25:04 +0200

    Corrected the unit tests for ft_strncpy:
    - Use of malloc/free
    - Right result for test 03

--- test/src/string/strncpy.c
+++ test/src/string/strncpy.c
@@ -6,7 +6,7 @@
 /*   By: adubois <adubois@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/26 14:03:37 by adubois           #+#    #+#             */
-/*   Updated: 2016/05/26 14:09:53 by adubois          ###   ########.fr       */
+/*   Updated: 2016/05/26 14:24:42 by adubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,11 @@
 static void	test_00_strncpy_EmptyString(void)
 {
 	char	src[] = "";
-	char	dst[10];
+	char	*dst = malloc(42);
 
 	v_assert_ptr(dst, ==, ft_strncpy(dst, src, 42));
 	v_assert_str(src, dst);
+	free(dst);
 
 	VTS;
 }
@@ -26,10 +27,11 @@ static void	test_00_strncpy_EmptyString(void)
 static void	test_01_strncpy_SimpleString(void)
 {
 	char	src[] = "Hello World!";
-	char	dst[20];
+	char	*dst = malloc(42);
 
 	v_assert_ptr(dst, ==, ft_strncpy(dst, src, 42));
 	v_assert_str(src, dst);
+	free(dst);
 
 	VTS;
 }
@@ -41,37 +43,42 @@ static void	test_02_strncpy_LongString(void)
 					"DQfAGv7N3wOJVN39hcFB PYv4inlqKOez87HVzR70 "
 					"199CZPuWfYAkXhehQkU1 6Hz1QcC72HJSygDsFLHX "
 					"hBbfUW4RY5FCeaXZwPPd jUus5zmEwkoh1FwCxUN3";
-	char	dst[300];
+	char	*dst = malloc(300);
 
 	v_assert_ptr(dst, ==, ft_strncpy(dst, src, 300));
 	v_assert_str(src, dst);
+	free(dst);
 
 	VTS;
 }
 
 static void	test_03_strncpy_LongStringLowerN(void)
 {
-	char	str[] = "iNVIVSvRyzGZuqmQo66i FjJZMkeSqQlmFVOsF1by "
+	char	src[] = "iNVIVSvRyzGZuqmQo66i FjJZMkeSqQlmFVOsF1by "
 					"7W4tCo99aiyrLxOggDp5 E9fN4ckgMUEs1sXvxEPn "
 					"DQfAGv7N3wOJVN39hcFB PYv4inlqKOez87HVzR70 "
 					"199CZPuWfYAkXhehQkU1 6Hz1QcC72HJSygDsFLHX "
 					"hBbfUW4RY5FCeaXZwPPd jUus5zmEwkoh1FwCxUN3";
-	char	dst[300];
+	char	*dst = malloc(300);
 
+	dst[42] = '*';
+	dst[43] = '\0';
 	v_assert_ptr(dst, ==, ft_strncpy(dst, src, 42));
-	v_assert_str("iNVIVSvRyzGZuqmQo66i FjJZMkeSqQlmFVOsF1by ", dst);
+	v_assert_str("iNVIVSvRyzGZuqmQo66i FjJZMkeSqQlmFVOsF1by *", dst);
+	free(dst);
 
 	VTS;
 }
 
 static void	test_04_strncpy_SimpleStringN0(void)
 {
-	char	str[] = "Hello World!";
-	char	dst[20];
+	char	src[] = "Hello World!";
+	char	*dst = malloc(42);
 
 	dst[0] = '\0';
 	v_assert_ptr(dst, ==, ft_strncpy(dst, src, 0));
 	v_assert_str("", dst);
+	free(dst);
 
 	VTS;
 }
