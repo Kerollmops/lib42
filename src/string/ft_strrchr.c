8fab265aa902a85e7d0a16b97dacbee3a82ea2e5
Alexandre Dubois <alexandre.dubois@goxheaven.ch>
2016-05-26 18:07:45 +0200

    Updated ft_strrchr: changed the way the loop decrements

--- src/string/ft_strrchr.c
+++ src/string/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: djean <djean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/27 12:14:45 by djean             #+#    #+#             */
-/*   Updated: 2016/05/18 19:04:49 by djean            ###   ########.fr       */
+/*   Updated: 2016/05/26 18:07:40 by adubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,8 @@ char	*ft_strrchr(const char *s, int c)
 	plc = (char *)s + ft_strlen(s);
 	if ((char)c == '\0')
 		return (plc);
-	--plc;
-	while (*plc)
-		if (*plc-- == (char)c)
-			return (++plc);
+	while (*(--plc))
+		if (*plc == (char)c)
+			return (plc);
 	return (NULL);
 }
