287caf516edbd67205f3ac9fbacc11c9bf8cc228
Darius Kramer <dev@dkramer.fastmail.com>
2016-09-26 22:55:17 +0200

    memory: memccpy, simplify code

--- src/memory/ft_memccpy.c
+++ src/memory/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: adubois <adubois@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/12 18:25:00 by adubois           #+#    #+#             */
-/*   Updated: 2016/05/25 18:00:44 by adubois          ###   ########.fr       */
+/*   Updated: 2016/09/26 22:31:43 by leonhart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,18 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	size_t	size;
 	char	*source;
 
-	if (n == 0)
-		return (NULL);
 	source = (char *)src;
 	size = 0;
-	while (source[size] != (char)c)
-	{
+	while (source[size] != (char)c && size != n)
 		++size;
-		if (size == n)
-		{
-			ft_memcpy(dest, src, n);
-			return (NULL);
-		}
+	if (size == n)
+	{
+		ft_memcpy(dest, src, n);
+		return (NULL);
 	}
-	ft_memcpy(dest, src, size);
-	return ((void *)((char *)dest + size));
+	else
+	{
+		ft_memcpy(dest, src, size);
+		return ((void *)((char *)dest + size));
+	}
 }
